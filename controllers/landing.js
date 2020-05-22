5b957b6b7b14050d6441e2199486729d9e4b9f22
Sam Gibson <samuelmgibson@gmail.com>
2020-05-22 12:30:40 -0700

    nding page post route to get email

--- controllers/landing.js
+++ controllers/landing.js
@@ -2,3 +2,8 @@
 exports.getLanding = function(req, res, next){
     res.render('landing', {title:'SMGBLOG'});
 }
+
+exports.submitLead = function(req, res, next){
+    console.log("lead email:", req.body.lead_email);
+    res.redirect('/');
+}
