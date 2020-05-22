7b4908e410a46f5971e58dc0f5c9337157a7337f
Sam Gibson <samuelmgibson@gmail.com>
2020-05-22 15:54:00 -0700

    additional sequelize stuff

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports ={
   "development": {
     "username": "express-mvp-dbuser",
-    "password": Splinyer7,
-    "database": "express-mvp-development",
+    "password": "Splinyer7",
+    "database": "express-mvp-db",
     "host": "127.0.0.1",
     "dialect": "postgres",
     "operatorsAliases": false,
