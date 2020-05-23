87fe3ff611359565d0b88ea15021a70b51dc6c27
Sam Gibson <samuelmgibson@gmail.com>
2020-05-23 12:25:13 -0700

    lead routes are protected by hasauth

--- routes/index.js
+++ routes/index.js
@@ -18,10 +18,9 @@ router.get('/logout', user.logout);
 router.get('/', landing.getLanding);
 router.post('/', landing.submitLead);
 router.get('/leads', hasAuth, landing.show_leads);
-router.get('/lead/:lead_id', landing.show_lead);
-router.get('/lead/:lead_id/edit', landing.show_edit_lead);
-router.post('/lead/:lead_id/edit', landing.edit_lead);
-router.post('/lead/:lead_id/delete', landing.delete_lead);
-
-router.post('/lead/:lead_id/delete-json', landing.delete_lead_json);
+router.get('/lead/:lead_id', hasAuth, landing.show_lead);
+router.get('/lead/:lead_id/edit', hasAuth, landing.show_edit_lead);
+router.post('/lead/:lead_id/edit', hasAuth, landing.edit_lead);
+router.post('/lead/:lead_id/delete', hasAuth, landing.delete_lead);
+router.post('/lead/:lead_id/delete-json', hasAuth, landing.delete_lead_json);
 module.exports = router;
